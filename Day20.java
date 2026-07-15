aec178f610111f03effa945d5420a3061103100b
Neeraj Katheriya <111559978+NeerajKatheriya1435@users.noreply.github.com>
2026-07-15 15:30:09 +0530

    Java crud operation file

---
+++ Day20.java
@@ -0,0 +1,60 @@
+import java.io.*;
+
+public class Day20 {
+    public static void main(String[] args) {
+
+//        try
+//        {
+//            File firstFile=new File("Ayush.txt");
+//            firstFile.createNewFile();
+//        } catch (IOException e) {
+////            throw new IOException(e);
+//            System.out.println("File can not be created");
+//        }
+
+//        File firstFile=new File("Ayush.txt");
+//        try
+//        {
+//            FileWriter fileWriter=new FileWriter("Ayush.txt",true); //Overrite
+//            fileWriter.write("Suman How are you Nice Looking");
+
+//            fileWriter.write("\nAman is Nice Guy");
+
+//            System.out.println("Line Executed");
+//            fileWriter.close();
+
+//            File firstFile=new File("Ayush.txt");
+//            BufferedReader bufferedReader=new BufferedReader(new FileReader(firstFile));
+//            System.out.println(bufferedReader.readLine());
+//            System.out.println(bufferedReader.lines());
+//            String str1;
+//            while ((str1=bufferedReader.readLine())!=null){
+//                System.out.println(str1+"\n");
+//            }
+//            bufferedReader.close();
+
+//        }catch (IOException e){
+//            System.out.println("Can not be wrritten");
+//        }
+
+//        try
+//        {
+//            File file = new File("Ayush.txt");
+//            BufferedReader br = new BufferedReader(new FileReader(file));
+//            StringBuilder str1=new StringBuilder();
+//
+//            String line;
+//            while ((line=br.readLine())!=null){
+//                str1.append(line.replace("Aman","Rohan")).append("\n");
+//            }
+//            System.out.println(str1);
+//            br.close();
+//            file.delete();
+//
+//        } catch (Exception e) {
+//            System.out.println("Can not be write");
+//        }
+
+
+    }
+}
